1e6a4a8606582cea1de80f7e944691d9e4ed78cb
Tom Harley <tharley87@gmail.com>
2017-07-09 12:35:02 -0400

    Tests: remove deprecated calls

--- test/functional/runs_controller_test.rb
+++ test/functional/runs_controller_test.rb
@@ -18,37 +18,42 @@ class RunsControllerTest < ActionController::TestCase
 
   test "should create run" do
     assert_difference('Run.count') do
-      post :create, run: { start_time_string: '2017-01-01 12:00',
-                           is_night: false,
-                           distance: 4.00,
-                           duration_string: '36:00',
-                           temperature: 72,
-                           weather_type_id: 2,
-                           elev_gain: 150,
-                           is_race: false }
+      post :create, params: {run: { start_time_string: '2017-01-01 12:00',
+                                    is_night: false,
+                                    distance: 4.00,
+                                    duration_string: '36:00',
+                                    temperature: 72,
+                                    weather_type_id: 2,
+                                    elev_gain: 150,
+                                    is_race: false }}
     end
 
     assert_redirected_to run_path(assigns(:run))
   end
 
   test "should show run" do
-    get :show, id: @run
+    get :show, params: {id: @run}
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @run
+    get :edit, params: {id: @run}
     assert_response :success
   end
 
   test "should update run" do
-    put :update, id: @run, run: { distance: @run.distance, duration: @run.duration, elev_gain: @run.elev_gain, start_time: @run.start_time, temperature: @run.temperature }
+    put :update, params: {id: @run,
+                          run: { distance: @run.distance,
+                                 duration: @run.duration,
+                                 elev_gain: @run.elev_gain,
+                                 start_time: @run.start_time,
+                                 temperature: @run.temperature }}
     assert_redirected_to run_path(assigns(:run))
   end
 
   test "should destroy run" do
     assert_difference('Run.count', -1) do
-      delete :destroy, id: @run
+      delete :destroy, params: {id: @run}
     end
 
     assert_redirected_to runs_path
