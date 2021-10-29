b08d7252478d77c74346bad00702cdd7394bcd8d
Tom Harley <tmharley@users.noreply.github.com>
2021-10-29 16:02:45 -0400

    Reworked shoe display

--- app/helpers/shoes_helper.rb
+++ app/helpers/shoes_helper.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 module ShoesHelper
   def shoe_size_clean(size)
     half = '&frac12;'
     "#{size.floor}#{half if Rational(size).denominator == 2}".html_safe
+  end
+
+  def colors_for_display(shoe_color)
+    style = "background: ##{shoe_color}"
+    style += '; color: black' if Color::RGB.by_hex(shoe_color).brightness > 0.75
+    style
   end
 end
